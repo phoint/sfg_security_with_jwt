b9b2425f2c87c6623439af56ed20886f022d6ffd
PhoiNT2806 <phoi.nt2806@gmail.com>
2023-02-22 09:28:01 +0700

    Init

---
+++ src/main/java/sfg/phoint/jwtsecure/JwtsecureApplication.java
@@ -0,0 +1,13 @@
+package sfg.phoint.jwtsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtsecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtsecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sfg/phoint/jwtsecure/JwtsecureApplicationTests.java
@@ -0,0 +1,13 @@
+package sfg.phoint.jwtsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtsecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
